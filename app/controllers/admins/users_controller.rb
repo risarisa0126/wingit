b61d4430f0087f5510ce5f276b1862679f78bb14
Your Name <you@example.com>
2020-01-21 04:26:04 +0000

    チャット機能

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -2,7 +2,7 @@ class Admins::UsersController < ApplicationController
 	before_action :authenticate_admin!
 
   def index
-  	@guides = Guides.all
-  	@tourists = Tourists.all
+  	@guides = Guide.all
+  	@tourists = Tourist.all
   end
 end
